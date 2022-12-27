2c5e55d95cecd0faef72f45416bfa52c297ef518
irfan <irfanfy@code.edu.az.com>
2022-12-28 02:22:07 +0400

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i =  16358;
+            int a = 4; int b = 2;
+            Console.WriteLine($"{a}{i}{b}");
+
+        }
+    }
+}
